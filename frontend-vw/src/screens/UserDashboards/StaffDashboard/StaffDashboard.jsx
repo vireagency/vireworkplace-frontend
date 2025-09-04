6e9f1826a2668b6323ea671454af42653ea9a269
JERRYRICHMAN007 <jerryrichman70@gmail.com>
2025-09-04 06:44:54 +0000

    refactor(staff): render nested routes via Outlet inside StaffDashboardLayout

--- frontend-vw/src/screens/UserDashboards/StaffDashboard/StaffDashboard.jsx
+++ frontend-vw/src/screens/UserDashboards/StaffDashboard/StaffDashboard.jsx
@@ -1,17 +1,18 @@
-import StaffDashboardLayout from "@/components/dashboard/StaffDashboardLayout"
-import { staffDashboardConfig } from "@/config/dashboardConfigs"
-import staffData from "./staffData.json"
+import { Outlet } from "react-router-dom";
+import StaffDashboardLayout from "@/components/dashboard/StaffDashboardLayout";
+import { staffDashboardConfig } from "@/config/dashboardConfigs";
+import staffData from "./staffData.json";
 
 export default function StaffDashboardPage() {
   return (
-    <StaffDashboardLayout 
+    <StaffDashboardLayout
       sidebarConfig={staffDashboardConfig}
       showSectionCards={true}
       showChart={true}
       showDataTable={true}
       dataTableData={staffData}
     >
-      {/* Staff-specific custom content can go here */}
+      <Outlet />
     </StaffDashboardLayout>
-  )
+  );
 }
